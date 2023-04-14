d3b9a840a3157334108582da1ac20b56200098aa
Alex Hastie <hastiea32@gmail.com>
2023-04-14 17:36:52 -0400

    Fixing infinite environment bug part 1

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -15,11 +15,6 @@ urlpatterns=[
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
 
     from django.conf import settings
 
